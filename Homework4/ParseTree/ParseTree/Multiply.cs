8f590a88d5557f515d2f498f2ecd333a01e9500e
rnretina <92212590+rnretina@users.noreply.github.com>
2022-07-28 03:21:16 +0300

    fix pull request review issues

---
+++ Homework4/ParseTree/ParseTree/Multiply.cs
@@ -0,0 +1,15 @@
+﻿namespace ParseTree;
+
+/// <summary>
+/// Represents multiply arithmetic operation
+/// </summary>
+public class Multiply : Operation
+{
+    public override void Calculate()
+    {
+        if (LeftOperation != null && RightOperation != null)
+        {
+            Result = LeftOperation.Result * RightOperation.Result;
+        }
+    }
+}
---
+++ Homework4/ParseTree/ParseTree/Plus.cs
@@ -0,0 +1,15 @@
+﻿namespace ParseTree;
+
+/// <summary>
+/// Represents plus arithmetic operation
+/// </summary>
+public class Plus : Operation
+{
+    public override void Calculate()
+    {
+        if (LeftOperation != null && RightOperation != null)
+        {
+            Result = LeftOperation.Result + RightOperation.Result;
+        }
+    }
+}
